56f88789491ae84755171d786f4c0c617be78d3d
Brigitte Hulliger <brigitte.hulliger@gmail.com>
2014-07-17 15:01:01 +0200

    dummy test

---
+++ scaffolding/app/web/client/application/signin/SigninPresenterTest.java
@@ -0,0 +1,26 @@
+package @package@;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SigninPresenterTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link ch.ims.todo.web.client.application.signin.SigninPresenter#onSignin()}.
+	 */
+	@Test
+	public void testOnSignin() {
+		// TODO Auto-generated method stub
+		assertTrue(true);
+	}
+
+}
